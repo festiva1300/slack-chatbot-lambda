1b0d84236487b27c9e93108fd412fe5b395e9a73
festiva1300 <3043378+festiva1300@users.noreply.github.com>
2023-04-26 08:51:14 +0900

    スレッドにはメンションなしで対応するよう変更

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ app = App(
     # Settings > Install App で取得可能な値
     token=os.environ["SLACK_BOT_TOKEN"],
 
-    # AWS Lamdba では、必ずこの設定を true にしておく必要があります
+    # AWS Lamdba では、必ずこの設定を true にしておく
     process_before_response=True,
 )
 
@@ -48,71 +48,140 @@ def respond_to_slack_within_3_seconds(body, ack):
     ack()
 
 
-def process_request(respond, body):
+def process_mention(respond, body):
     # メンションのメッセージに対して返信する
     logging.debug(body)
+    if "event" not in body:
+        return
 
-    ts = body["event"]["ts"]
-    prompt = body["event"]["text"]
+    # 会話履歴の取得
+    thread_ts = body["event"]["ts"]
+    if "thread_ts" in body["event"]:
+        # スレッドの途中でメンションされた場合
+        thread_ts = body["event"]["thread_ts"]
     channel_id = body["event"]["channel"]
+    history_id = f"{channel_id}:{thread_ts}"
+    history = get_history(history_id)
+    if len(history) > 0:
+        # 会話履歴がある場合はリプライメッセージ処理で対応(何もしない)
+        return 
+
+    start_timestamp = int(time.time())
+
+    # プロンプトの作成
+    message = body["event"]["text"]
     user_id = body["event"]["user"]
-    answer = send_prompt(user_id, prompt)
-    post_message(channel_id, ts, f"<@{user_id}> {answer}")
+    prompt = create_prompt([], message)
+    
+    # 回答の取得とSlackへの送信
+    answer = send_prompt(prompt)
+    post_message(channel_id, thread_ts, f"<@{user_id}> {answer}")
+
+    # 会話履歴の保存
+    save_history(history_id, message, answer, start_timestamp)
 
 
-def post_message(channel_id, thread_td, message):
+def process_message(respond, body):
+    # リプライに対して返信する
+    if "event" not in body:
+        return
+
+    if "thread_ts" not in body["event"]:
+        # 親メッセージには反応しない
+        return 
+
+    start_timestamp = int(time.time())
+
+    # 会話履歴の取得
+    thread_ts =  body["event"]["thread_ts"]
+    channel_id = body["event"]["channel"]
+    history_id = f"{channel_id}:{thread_ts}"
+    history = get_history(history_id)
+    if len(history) == 0:
+        # 会話履歴がないスレッドには対応しない
+        return 
+
+    # プロンプトの作成
+    message = body["event"]["text"]
+    user_id = body["event"]["user"]
+    prompt = create_prompt(history, message)
+
+    # 回答の取得とSlackへの送信
+    answer = send_prompt(prompt)
+    post_message(channel_id, thread_ts, f"<@{user_id}> {answer}")
+
+    # 会話履歴の保存
+    save_history(history_id, message, answer, start_timestamp)
+
+
+def save_history(history_id, message, answer, start_timestamp):
+        # dynamoDBにプロンプトを保存
+        items = [
+            {
+                'id': history_id,
+                'timestamp': start_timestamp,
+                'role': 'user',
+                'content': message
+            }
+        ]
+
+        # dynamoDBに回答を挿入
+        items.append({
+            'id': history_id,
+            'timestamp': int(time.time()),
+            'role': 'assistant',
+            'content': answer
+        })
+        with table.batch_writer() as batch:
+            for item in items:
+                batch.put_item(Item=item)
+
+
+def get_history(history_id):
+    # 24時間前のUnix Time
+    one_day_ago = int(time.time()) - 24 * 60 * 60
+
+    # dynamoDBから過去履歴を取得
+    response = table.query(
+        KeyConditionExpression="id = :id and #ts >= :timestamp",
+        ExpressionAttributeNames={"#ts": "timestamp"},
+        ExpressionAttributeValues={
+            ":id": history_id,
+            ":timestamp": one_day_ago
+        },
+        ScanIndexForward=False, 
+        Limit=MAX_HISTORY
+    )
+    return response['Items']
+
+
+def create_prompt(history, message):
+    # 過去履歴からメッセージを生成
+    messages = [{"role": "system", "content": SYSTEM_CONTENT}]
+    for item in history:
+        messages.append({"role": item["role"], "content": item["content"]})
+
+    messages.append({"role": "user", "content": message})
+
+    return messages
+
+
+def post_message(channel_id, thread_ts, message):
     # スレッドに対してメッセージを返信する
     try:
         result = app.client.chat_postMessage(
             channel=channel_id,
-            thread_ts=thread_td,
-            text=message,
-            reply_broadcast = True
+            thread_ts=thread_ts,
+            text=message
         )
     except Exception as e:
         logging.error("Error sending postMessage event: {}".format(e))
 
-def send_prompt(user_id, prompt=''):
+def send_prompt(messages):
+    logging.debug(messages)
     # 対話履歴と最新のメンションの内容からプロンプトを作成してチャットモデルに送信する
 
-	# promptがない場合
-    if not prompt:
-        return
-
-    answer = ''
-
     try:        
-        # 24時間前のUnix Time
-        one_day_ago = int(time.time()) - 24 * 60 * 60
-
-        # dynamoDBにプロンプトを保存
-        items = [
-            {
-                'id': user_id,
-                'timestamp': int(time.time()),
-                'role': 'user',
-                'content': prompt
-            }
-        ]
-
-        # dynamoDBから過去履歴を取得
-        response = table.query(
-            KeyConditionExpression="id = :id and #ts >= :timestamp",
-            ExpressionAttributeNames={"#ts": "timestamp"},
-            ExpressionAttributeValues={
-                ":id": user_id,
-                ":timestamp": one_day_ago
-            },
-            ScanIndexForward=False, 
-            Limit=MAX_HISTORY
-        )
-
-        # 過去履歴からメッセージを生成
-        messages = [{"role": "system", "content": SYSTEM_CONTENT}]
-        for item in response['Items']:
-            messages.append({"role": item["role"], "content": item["content"]})
-
-        messages.append({"role": "user", "content": prompt})
 
         # 回答を生成
         response = openai.ChatCompletion.create(
@@ -123,16 +192,9 @@ def send_prompt(user_id, prompt=''):
         )
         answer = response['choices'][0]['message']['content']
 
-        # dynamoDBに回答を挿入
-        items.append({
-            'id': user_id,
-            'timestamp': int(time.time()),
-            'role': 'assistant',
-            'content': answer
-        })
-        with table.batch_writer() as batch:
-            for item in items:
-                batch.put_item(Item=item)
+ 
+    except openai.error.Timeout as e:
+        answer = f"タイムアウトが発生しました: {str(e)}"
 
     except Exception as e:
         answer = f"例外が発生しました: {str(e)}"
@@ -140,7 +202,10 @@ def send_prompt(user_id, prompt=''):
     return answer
 
 # メンションに反応するように設定
-app.event("app_mention")(ack=respond_to_slack_within_3_seconds, lazy=[process_request])
+app.event("app_mention")(ack=respond_to_slack_within_3_seconds, lazy=[process_mention])
+
+# メッセージに反応するように設定
+app.message()(ack=respond_to_slack_within_3_seconds, lazy=[process_message])
 
 if __name__ == "__main__":
     # python app.py のように実行すると開発用 Web サーバーで起動します
